45f729012c0c3129eb960ae579e752753c03310b
Mochamad Iqbal Dwi Cahyo <razeluxe20@gmail.com>
2018-10-29 10:07:17 +0700

    CHANGELOG v0.0.2: - Add base project with clean architecture

--- main.go
+++ main.go
@@ -1,21 +1,20 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
+	"net/url"
 	"os"
 	"time"
 
-	_ "github.com/go-sql-driver/mysql"
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/postgres"
-	"github.com/labstack/echo"
+	httpDeliver "github.com/mochadwi/go-article/article/delivery/http"
 	articleRepo "github.com/mochadwi/go-article/article/repository"
 	articleUcase "github.com/mochadwi/go-article/article/usecase"
 	"github.com/mochadwi/go-article/middleware"
-	"github.com/mochadwi/go-article/models"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/labstack/echo"
 	"github.com/spf13/viper"
-	httpDeliverIris "github.com/mochadwi/go-article/article/delivery/httpiris"
 )
 
 func init() {
@@ -39,36 +38,30 @@ func main() {
 	dbUser := viper.GetString(`database.user`)
 	dbPass := viper.GetString(`database.pass`)
 	dbName := viper.GetString(`database.name`)
-	connection := fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s sslmode=disable", dbUser, dbPass, dbHost, dbPort, dbName)
-	//fmt.Println(connection)
-	dbConn, err := gorm.Open(`postgres`, connection)
-	dbConn.LogMode(true)
+	connection := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPass, dbHost, dbPort, dbName)
+	val := url.Values{}
+	val.Add("parseTime", "1")
+	val.Add("loc", "Asia/Jakarta")
+	dsn := fmt.Sprintf("%s?%s", connection, val.Encode())
+	dbConn, err := sql.Open(`mysql`, dsn)
 	if err != nil && viper.GetBool("debug") {
 		fmt.Println(err)
 	}
-
-	err = dbConn.DB().Ping()
+	err = dbConn.Ping()
 	if err != nil {
 		log.Fatal(err)
 		os.Exit(1)
 	}
 
-	// TODO: Find better approach to close DB connection
-	//defer dbConn.Close()
-
-	// Migrate the schema
-	dbConn.AutoMigrate(&models.Article{})
-
+	defer dbConn.Close()
 	e := echo.New()
 	middL := middleware.InitMiddleware()
 	e.Use(middL.CORS)
-	//ar := articleRepo.NewMysqlArticleRepository(dbConn)
-	ar := articleRepo.NewGormsqlArticleRepository(dbConn)
+	ar := articleRepo.NewMysqlArticleRepository(dbConn)
 
 	timeoutContext := time.Duration(viper.GetInt("context.timeout")) * time.Second
 	au := articleUcase.NewArticleUsecase(ar, timeoutContext)
-	//httpDeliverEcho.NewArticleHttpEchoHandler(e, au)
-	httpDeliverIris.NewArticleHttpIrisHandler(e, au)
+	httpDeliver.NewArticleHttpHandler(e, au)
 
 	e.Start(viper.GetString("server.address"))
 }
