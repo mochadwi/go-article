e75fe85c6f2db919c7923d5168ea26908f42a136
Mochamad Iqbal Dwi Cahyo <razeluxe20@gmail.com>
2018-10-31 07:21:55 +0700

    Changelog:
    - Prioritize check article id, then check for existed item

--- article/usecase/article_ucase.go
+++ article/usecase/article_ucase.go
@@ -59,14 +59,14 @@ func (a *articleUsecase) Update(c context.Context, ar *models.Article) (*models.
 	ctx, cancel := context.WithTimeout(c, a.contextTimeout)
 	defer cancel()
 
-	existedArticle, _ := a.GetByTitle(ctx, ar.Title)
-	if existedArticle != nil {
-		return nil, models.CONFLIT_ERROR
-	}
-
 	_, err := a.GetByID(ctx, ar.ID)
 	if err != nil {
 		return nil, models.NOT_FOUND_ERROR
+	}
+
+	existedArticle, _ := a.GetByTitle(ctx, ar.Title)
+	if existedArticle != nil {
+		return nil, models.CONFLIT_ERROR
 	}
 
 	ar.UpdatedAt = time.Now()
