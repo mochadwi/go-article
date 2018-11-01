d1e7b4bae97fae9f889ab0e8018527afc264aa8f
Mochamad Iqbal Dwi Cahyo <mochadwi@users.noreply.github.com>
2018-11-01 10:52:09 +0700

    Update install.sh
    update instruction to install gin and add docs

--- install.sh
+++ install.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+# install gin auto-reload web server for golang
+go get github.com/codegangsta/gin
+
+# verify installation of gin
+gin -h
+
+# install deps of this project
 dep ensure
+
+# build binary
 make
-gin -a 3333 article_clean # auto-reload
+
+# auto-reload, change your port
+gin -a 3333 article_clean
