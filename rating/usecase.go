698201f66776d253320c739c0f699504d9fd0c89
Mochamad Iqbal Dwi Cahyo <mochadwi20@gmail.com>
2019-05-31 00:58:21 +0700

    Changelog - Add rating repo and usecase - Refactor variable naming to be readable - Add UUID for requestID - Remove unnecessary method on rating repo and usecase

--- rating/usecase.go
+++ rating/usecase.go
@@ -8,9 +8,5 @@ import (
 
 type RatingUsecase interface {
 	Create(context.Context, *model.Rating) (*model.Rating, error)
-	GetAll(ctx context.Context, cursor string, num int64) (*[]*model.Rating, string, error)
-	GetByID(ctx context.Context, id int64) (*model.Rating, error)
-	GetByTitle(ctx context.Context, title string) (*model.Rating, error)
-	Update(ctx context.Context, ar *model.Rating) (*model.Rating, error)
-	Delete(ctx context.Context, id int64) (bool, error)
+	GetByID(ctx context.Context, lessonId int64) (*model.Rating, error)
 }
